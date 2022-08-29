3bfa146c0c1680aee0888628f3802109f86471d7
sggnology <sggnology@gmail.com>
2022-08-29 16:07:51 +0900

    [init] generate project

---
+++ src/main/kotlin/com/springmvcstudy/springmvcstudy/SpringMvcStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.springmvcstudy.springmvcstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvcStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/springmvcstudy/springmvcstudy/SpringMvcStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springmvcstudy.springmvcstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMvcStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
